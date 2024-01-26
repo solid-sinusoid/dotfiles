337edb650a5bade99ddc5af1ead7bc2b78987589
Bill Finger <ur.narmak@gmail.com>
2024-01-26 18:57:13 +0300

    u

--- run_once_oh_my_zsh.sh
+++ run_once_oh_my_zsh.sh
@@ -4,6 +4,8 @@ if [! -d "$HOME/.oh-my-zsh/"]; then
   echo "Setup oh-my-zsh"
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
   echo "Install additon plugins"
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 else
   echo "oh-my-zsh is alredy installed!"
 fi
